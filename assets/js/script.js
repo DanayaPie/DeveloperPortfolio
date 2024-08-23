94af6d00c3b2a65dd6ef9a689551525d431591a1
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-08-23 19:22:53 -0400

    Click outside hamburgerMenu will hide the menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
-function hamburgerMenu() {
+function toggleHamburgerMenu() {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const hamburgerLinks = document.querySelector('.hamburger-links');
 
-    hamburgerBtn.classList.toggle("open");
-    hamburgerLinks.classList.toggle("open");
+    hamburgerBtn.classList.toggle("active");
+    hamburgerLinks.classList.toggle("active");
+
+    // if onclick is on on hamburgerBtn and not on hamburgerLinks, close hamburger menu
+    document.onclick = function (e) {
+        if (!hamburgerBtn.contains(e.target) && !hamburgerLinks.contains(e.target)) {
+            hamburgerBtn.classList.remove("active");
+            hamburgerLinks.classList.remove("active");
+        }
+    }
 };
 
