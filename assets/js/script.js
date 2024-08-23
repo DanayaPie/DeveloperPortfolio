04f43fc6beaeff07f8f0e688cb2f4a8a2cb259f3
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-08-22 21:31:14 -0400

    Separate hamburger nav from navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-const hamburgerBtn = document.getElementsByClassName('hamburger-btn')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
 // if hamburger button is active, navbar links will show
+const hamburgerBtn = document.getElementsByClassName('hamburger-btn')[0];
+const hamburgerLinks = document.getElementsByClassName('hamburger-links')[0];
+
 hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle('active')
-    navbarLinks.classList.toggle('active')
+    hamburgerLinks.classList.toggle('active')
 });
+
 
 
