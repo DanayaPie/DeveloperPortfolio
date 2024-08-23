54e19dc509b289d42f7d1318d7f20040bb960db1
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-08-23 18:31:11 -0400

    hamburger nav styling and animation fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-// if hamburger button is active, navbar links will show
-const hamburgerBtn = document.getElementsByClassName('hamburger-btn')[0];
-const hamburgerLinks = document.getElementsByClassName('hamburger-links')[0];
+function hamburgerMenu() {
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const hamburgerLinks = document.querySelector('.hamburger-links');
 
-hamburgerBtn.addEventListener('click', () => {
-    hamburgerBtn.classList.toggle('active')
-    hamburgerLinks.classList.toggle('active')
-});
-
-
+    hamburgerBtn.classList.toggle("open");
+    hamburgerLinks.classList.toggle("open");
+};
 
