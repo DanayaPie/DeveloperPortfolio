4f1fa7412c257d377b81a229758d6b416b43e16c
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-08-28 14:37:10 -0400

    add highlight active section in full-nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,43 @@
+// Toggle Hamburger Menu
 function toggleHamburgerMenu() {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const hamburgerLinks = document.querySelector('.hamburger-links');
 
-    hamburgerBtn.classList.toggle("active");
-    hamburgerLinks.classList.toggle("active");
+    hamburgerBtn.classList.toggle('active');
+    hamburgerLinks.classList.toggle('active');
 
     // if onclick is on on hamburgerBtn and not on hamburgerLinks, close hamburger menu
     document.onclick = function (e) {
         if (!hamburgerBtn.contains(e.target) && !hamburgerLinks.contains(e.target)) {
-            hamburgerBtn.classList.remove("active");
-            hamburgerLinks.classList.remove("active");
+            hamburgerBtn.classList.remove('active');
+            hamburgerLinks.classList.remove('active');
         }
     }
 };
+
+// Mark Nav Links as 'active' when click or scroll to each section
+document.addEventListener("DOMContentLoaded", function () {
+
+    let sections = document.querySelectorAll('.child-section');
+    let navLinks = document.querySelectorAll('.nav-links a');
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 200;
+            let height = sec.offsetHeight - 200;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+
+                    links.classList.remove('active');
+                    document.querySelector('.nav-links a[href*=' + id + ']').classList.add('active');
+                });
+            };
+
+        });
+    };
+});
 
