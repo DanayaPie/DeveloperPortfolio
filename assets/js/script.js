57210a3b510f898c1e5fa410079796d1395a11b4
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-09-11 11:15:45 -0400

    modularized functions and added skills section template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Toggle Hamburger Menu
+// TOGGLE HAMBURGER MENU
 function toggleHamburgerMenu() {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const hamburgerLinks = document.querySelector('.hamburger-links');
@@ -16,9 +16,18 @@ function toggleHamburgerMenu() {
 };
 
 
-// Mark nav-links as 'active' when click or scroll to each section
+
+// EVENT LISTENER
 document.addEventListener("DOMContentLoaded", function () {
 
+    handleScrollNavigation(); // Initialized scroll navigation
+    displaySkills(); // Display all skills by default
+});
+
+
+
+// SCROLL NAVIGATION
+function handleScrollNavigation() {
     let sections = document.querySelectorAll('.child-section');
     let navLinks = document.querySelectorAll('.nav-links a');
 
@@ -45,17 +54,89 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     };
-});
+};
 
 
-// Show/Hide Read More
+
+// SHOW / HIDE READ MORE SECTION
+
 function showReadMore() {
     document.querySelector('#read-more').style.display = 'flex';
     document.querySelector('.read-more-btn').style.display = 'none';
-}
+};
 
 function hideReadMore() {
     document.querySelector('#read-more').style.display = 'none';
     document.querySelector('.read-less-btn').style.display = 'flex';
     document.querySelector('.read-more-btn').style.display = 'flex';
-}
+};
+
+
+
+// SKILLS SECTION
+
+function getSkills() {
+    return [
+        // Languages
+        { name: 'Java', image: 'java.png', categories: ['Languages'], ranking: 1 },
+        { name: 'Python', image: 'python.png', categories: ['Languages'], ranking: 2 },
+        { name: 'JavaScript', image: 'javascript.png', categories: ['Languages', 'Web Development'], ranking: 3 },
+        { name: 'SQL', image: 'sql.png', categories: ['Languages', 'Databases'], ranking: 4 },
+
+        // Web Development
+        { name: 'HTML', image: 'html.png', categories: ['Web Development'], ranking: 1 },
+        { name: 'CSS', image: 'css.png', categories: ['Web Development'], ranking: 2 },
+
+        // Databases
+        { name: 'PostgreSQL', image: 'postgresql.png', categories: ['Databases'], ranking: 1 },
+        { name: 'MongoDB', image: 'mongodb.png', categories: ['Databases'], ranking: 2 },
+        { name: 'H2', image: 'h2.png', categories: ['Databases'], ranking: 3 },
+
+        // Frameworks and Libraries
+        { name: 'Spring', image: 'spring.png', categories: ['Frameworks and Libraries'], ranking: 1 },
+        { name: 'Hibernate', image: 'hibernate.png', categories: ['Frameworks and Libraries'], ranking: 2 },
+
+        // Test and Automation
+        { name: 'Selenium', image: 'selenium.png', categories: ['Testing and Automation'], ranking: 1 },
+        { name: 'JUnit 5', image: 'junit5.png', categories: ['Testing and Automation'], ranking: 2 },
+        { name: 'Mockito', image: 'mockito.png', categories: ['Testing and Automation'], ranking: 3 },
+        { name: 'Cucumber', image: 'cucumber.png', categories: ['Testing and Automation'], ranking: 4 },
+        { name: 'Postman', image: 'postman.png', categories: ['Testing and Automation', 'DevOps'], ranking: 5 },
+
+        // DevOps
+        { name: 'Git', image: 'git.png', categories: ['DevOps'], ranking: 1 },
+        { name: 'Jenkins', image: 'jenkins.png', categories: ['DevOps'], ranking: 2 },
+        { name: 'Maven', image: 'maven.png', categories: ['DevOps'], ranking: 3 },
+        { name: 'Jira', image: 'jira.png', categories: ['DevOps'], ranking: 4 },
+
+        // Tools
+        { name: 'DBeaver', image: 'dbeaver.png', categories: ['Tools'], ranking: 1 },
+        { name: 'Lucidchart', image: 'lucidchart.png', categories: ['Tools'], ranking: 2 },
+        { name: 'Draw.io', image: 'drawio.png', categories: ['Tools'], ranking: 3 }
+    ]
+};
+
+
+function displaySkills(filterCategory = null) {
+    let skills = getSkills();
+
+    const skillCont = document.getElementsByClassName('skill-container')[0];
+    skillCont.innerHTML = ''; // clear current content
+
+    skills.forEach(skill => {
+        const img = new Image();
+        img.src = 'assets/images-files/skills/${skill.image}';
+        img.alt = skill.name;
+        // img.classList.add('enlarge-image');
+
+        const skillCardDiv = document.createElement('div');
+        skillCardDiv.classList.add('image-card');
+        skillCardDiv.appendChild(img);
+
+        const skillName = document.createElement('p');
+        skillName.textContent = skill.name;
+        skillCardDiv.appendChild(skillName);
+
+        skillCont.appendChild(skillCardDiv);
+    });
+};
