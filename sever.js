a92e373ad48528907b9d411657bff2af2c00f43e
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-11-04 15:02:00 -0500

    added 400 error message

--- sever.js
+++ sever.js
@@ -8,6 +8,11 @@ const port = 3000;
 // Serve static files (HTML, CSS, JS) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 400 Error message
+app.use(function (req, res) {
+    res.status(400);
+    return res.send(`404 Error: Resource not found`);
+});
 
 // Start the server
 app.listen(port, () => {
